1b2a098c6f77135c4e21d919ab74bbc1235e29fc
rafa907p <rafael.afp01@gmail.com>
2026-03-13 00:30:52 +0000

    Paths and Viewing Setup
    Small considerations, viewing images side by side given relative paths

---
+++ config.py
@@ -0,0 +1,6 @@
+DATA_DIR = r"../deepfake_data"
+
+WIKI_DIR = DATA_DIR + "/wiki"
+INPAINTING_DIR = DATA_DIR + "/inpainting"
+INSIGHT_DIR = DATA_DIR + "/insight"
+TEXT2IMG_DIR = DATA_DIR + "/text2img"
